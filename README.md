33e248f8aadff304a8b4fa9cf5f441dc5848ad39
Benedikt Lorch <benedikt.lorch@fau.de>
2017-07-01 15:05:37 +0200

    Add substitute method that inserts Chuck Norris jokes into alternative tags

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Image caption replacement
-Fills empty image altnerative tags with life to make screen reading great again for visually impaired people
+Fills empty image alternative tags with life to make screen reading great again for visually impaired people
+
+# Prerequisites
+Note: For developers only.
+
+Install and enable the [https://addons.mozilla.org/de/firefox/addon/cors-everywhere/](CORS Everywhere Add-on)
+
+To disable blocking of mixed http/https content:
+Go to (about:config) and adjust the following settings
+* Set `security.mixed_content.block_active_content` to `false`
+* Set `security.mixed_content.block_display_content` to `true`
--- substitute.js
+++ substitute.js
@@ -1,13 +1,33 @@
-function insertCaption(node) {
-  if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === "IMG") {
-    console.log("Found image with alt=\"" + "\"");
-    return;
-  }
+function insertChuckNorrisJoke(imageTag) {
+  const requestURL = "http://api.icndb.com/jokes/random";
+  const requestHeaders = new Headers();
+  //requestHeaders.append('Authorization', 'Bearer ' + accessToken);
 
-  for (let i = 0; i < node.childNodes.length; i++) {
-    insertCaption(node.childNodes[i]);
-  }
+  const driveRequest = new Request(requestURL, {
+    method: "GET",
+    headers: requestHeaders,
+    mode: 'no-cors'
+  });
+
+  return fetch(driveRequest).then((response) => {
+    if (response.status === 200) {
+      promise = response.text();
+      promise.then((jsonResult) => {
+        const result = JSON.parse(jsonResult);
+        const success = result["type"] === "success";
+        var joke = result["value"]["joke"];
+        imageTag.setAttribute("alt", joke);
+      });
+    } else {
+      alert("Failed to fetch request");
+      console.log("Failed to fetch request");
+      console.log(response.status);
+    }
+  });
 }
 
-// Start the recursion from the body tag.
-insertCaption(document.body);
+// TODO update tags that are loaded on runtime
+var imageTags = document.body.getElementsByTagName("img");
+for (var i=0; i<imageTags.length; i++) {
+  insertChuckNorrisJoke(imageTags[i]);
+}
