d99e11f29e4f83c35bef41d88f3de7404c84650a
Benedikt Lorch <benedikt.lorch@fau.de>
2017-07-02 01:00:07 +0200

    Connect to Vision API. Retrieve subscription key from options

--- options.js
+++ options.js
@@ -1,29 +1,25 @@
 function saveOptions(e) {
   e.preventDefault();
+
+  var subscriptionKey = document.querySelector("#subscriptionKey").value;
+
   browser.storage.local.set({
-    username: document.querySelector("#username").value,
-    key: document.querySelector("#key").value
+    "subscriptionKey": subscriptionKey
   });
 }
 
 function restoreOptions() {
 
-  function setCurrentUsername(result) {
-    document.querySelector("#username").value = result.username || "INSERT API USERNAME HERE";
-  }
-
-  function setCurrentKey(result) {
-    document.querySelector("#key").value = result.key || "INSERT API KEY HERE";
+  function setSubscriptionKey(result) {
+    document.querySelector("#subscriptionKey").value = result.subscriptionKey || ""
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  var getUsername = browser.storage.local.get("username");
-  var getKey = browser.storage.local.get("key");
-  getUsername.then(setCurrentUsername, onError);
-  getKey.then(setCurrentKey, onError);
+  var getSubscriptionKey = browser.storage.local.get("subscriptionKey");
+  getSubscriptionKey.then(setSubscriptionKey, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
--- substitute.js
+++ substitute.js
@@ -1,63 +1,95 @@
-function insertChuckNorrisJoke(imageTag) {
-  const requestURL = "http://api.icndb.com/jokes/random";
-  const requestHeaders = new Headers();
-  //requestHeaders.append('Authorization', 'Bearer ' + accessToken);
+/***  Helper functions ***/
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
 
+function urlSerialize(obj) {
+  var str = [];
+  for(var p in obj)
+     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+  return str.join("&");
+}
+
+function hasProperty(jsonObject, path) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var obj = jsonObject;
+  for (var i=0; i<args.length; i++) {
+    var arg = args[i];
+    // Is the current object an array?
+    if (Number.isInteger(arg)) {
+      if (!Array.isArray(obj) || arg < 0 || arg >= obj.length) {
+        return false;
+      }
+    }
+    // Otherwise parse it as an object
+    else if (!obj || !obj.hasOwnProperty(args[i])) {
+      return false;
+    }
+    obj = obj[args[i]];
+  }
+  return true;
+}
+
+/*** Alternative tag substitution ***/
+function insertImageCaption(imageTag, subscriptionKey) {
+  var uriBase = "http://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze";
+
+  // Request parameters.
+  var params = {
+      "visualFeatures": "Categories,Description",
+      "language": "en",
+  };
+
+  // src attribute will always give the full url
+  var sourceImageUrl = imageTag.src;
+  
+  // Set request headers and body
+  const requestHeaders = new Headers();
+  requestHeaders.append('Content-Type', 'application/json');
+  requestHeaders.append("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+  const requestURL = uriBase + "?" + urlSerialize(params);
   const driveRequest = new Request(requestURL, {
-    method: "GET",
+    method: "POST",
     headers: requestHeaders,
-    mode: 'no-cors'
+    mode: 'cors',
+    body: '{"url": "' + sourceImageUrl + '"}'
   });
 
+  // Perform API call
   return fetch(driveRequest).then((response) => {
     if (response.status === 200) {
       promise = response.text();
       promise.then((jsonResult) => {
         const result = JSON.parse(jsonResult);
-        const success = result["type"] === "success";
-        var joke = result["value"]["joke"];
-        imageTag.setAttribute("alt", joke);
+        if (!hasProperty(result, "description", "captions", 0, "text")) {
+          console.log("Response does not have a caption");
+          return;
+        }
+        const text = result["description"]["captions"][0]["text"];
+        imageTag.setAttribute("alt", text);
       });
     } else {
-      alert("Failed to fetch request");
-      console.log("Failed to fetch request");
-      console.log(response.status);
+      console.log("Failed to fetch request " + response.status);
     }
+  }).catch(function(response) {
+    console.log(response);
   });
 }
 
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
 function loadPreferences(onFinishedCallback, onErrorCallback) {
-  function onPreferencesLoaded() {
-    if (username === "" || key === "") {
-      return;
-    }
-
-    onFinishedCallback(username, key);
-  }
-
-  var username = "";
-  var key = "";
-  browser.storage.local.get("username").then((result) => {
-    username = result.username;
-    onPreferencesLoaded();
-  }, onErrorCallback);
-  browser.storage.local.get("key").then((result) => {
-    key = result.key;
-    onPreferencesLoaded();
+  browser.storage.local.get("subscriptionKey").then((response) => {
+    onFinishedCallback(response.subscriptionKey);
   }, onErrorCallback);
 }
 
-function insertJokes(username, key) {
+function insertImageCaptions(subscriptionKey) {
   // TODO update tags that are loaded on runtime
   var imageTags = document.body.getElementsByTagName("img");
   for (var i=0; i<imageTags.length; i++) {
-    insertChuckNorrisJoke(imageTags[i]);
+    insertImageCaption(imageTags[i], subscriptionKey);
   }
 }
 
-// Main method:
-loadPreferences(insertJokes, onError);
+// Main method
+loadPreferences(insertImageCaptions, onError);
